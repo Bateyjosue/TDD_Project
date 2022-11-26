d4c93a10163c162d1a2857e76ab12bb48103ade3
Bateyjosue <josuebatey19@gmail.com>
2022-11-26 17:02:08 +0200

    Add fizzbuzz function TDD

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -24,7 +24,24 @@ describe Solver do
       expect(@solver.reverse("race")).to eql("ecar")
     end
   end
-
+  
   context "fizzbuzz function Tester" do
+    it "Should return fizzbuzz for 15" do
+      expect(@solver.fizzbuzz(15)).to eql("fizzbuzz")
+    end
+
+    it "Should return fizz for 3" do
+      expect(@solver.fizzbuzz(3)).to eql("fizz")
+    end
+
+    it "Should return buzz for 5" do
+      expect(@solver.fizzbuzz(5)).to eql("buzz")
+    end
+
+    it "Should return 7 for 7" do
+      expect(@solver.fizzbuzz(7)).to eql(7)
+    end
   end
-end
+  end
+
+
