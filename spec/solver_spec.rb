73fa0ba717f2494d0ec5dab3467faf77109a3820
Alier Philip <alierditmaguet@gmail.com>
2022-11-26 18:29:57 +0200

    Add test to factorial method that raise exception

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,47 +1,45 @@
-require './solver'
+require 'solver'
 
 describe Solver do
-  before :each do
-    @solver = Solver.new
+  describe '#factorial' do
+  it 'returns the factorial of a number' do
+    solver = Solver.new
+    expect(solver.factorial(5)).to eq(120)
   end
 
-  context 'Factorial of N integer Tester' do
-    it 'Should return the factorial of 5' do
-      expect(@solver.factorial).to be 120
-    end
-    it 'Should return the factorial of 11' do
-      expect(@solver.factorial).to be 39_916_800
-    end
-    it 'Should return the factorial of 3' do
-      expect(@solver.factorial).to be 6
-    end
-  end
-
-  context 'Reverse of a string Tester' do
-    it 'Should reverse the string hello to olleh' do
-      expect(@solver.reverse('hello')).to eql('olleh')
-    end
-
-    it 'Should reverse the string race to ecar' do
-      expect(@solver.reverse('race')).to eql('ecar')
-    end
-  end
-
-  context 'fizzbuzz function Tester' do
-    it 'Should return fizzbuzz for 15' do
-      expect(@solver.fizzbuzz(15)).to eql('fizzbuzz')
-    end
-
-    it 'Should return fizz for 3' do
-      expect(@solver.fizzbuzz(3)).to eql('fizz')
-    end
-
-    it 'Should return buzz for 5' do
-      expect(@solver.fizzbuzz(5)).to eql('buzz')
-    end
-
-    it 'Should return 7 for 7' do
-      expect(@solver.fizzbuzz(7)).to eql(7)
-    end
+  it 'raises an error if a negative number is given' do
+    solver = Solver.new
+    expect { solver.factorial(-1) }.to raise_error(ArgumentError)
   end
 end
+
+  describe 'Reverse of a string Tester' do
+    it 'Should return  reverse of a word' do
+      solver = Solver.new
+  expect(solver.reverse('hello')).to eql('olleh')
+    end
+  end
+  
+ describe 'fizzbuzz function Tester' do
+  it 'returns "Fizz" if the number is divisible by 3' do
+    solver = Solver.new
+    expect(solver.fizzbuzz(3)).to eql('Fizz')
+  end
+
+  it 'returns "Buzz" if the number is divisible by 5' do
+    solver = Solver.new
+    expect(solver.fizzbuzz(5)).to eql('Buzz')
+  end
+
+  it 'returns "FizzBuzz" if the number is divisible by 15' do
+    solver = Solver.new
+    expect(solver.fizzbuzz(15)).to eql('FizzBuzz')
+  end
+
+  it 'returns the number as a string if the number is not divisible by 3 or 5' do
+    solver = Solver.new
+    expect(solver.fizzbuzz(7)).to eq('7')
+  end
+end
+end
+
